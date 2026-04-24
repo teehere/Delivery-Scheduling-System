7572ed445165df057e0ec4feb3a7c89296a2231a
TEE LE XUAN <teelexuan@gmail.com>
2026-04-24 21:06:05 +0800

    updated

--- UECS2083_AM_05/src/controller/TestDeliveryApp.java
+++ UECS2083_AM_05/src/controller/TestDeliveryApp.java
@@ -2,7 +2,7 @@ package controller;
 
 public class TestDeliveryApp {
 
-	public static void main(String[] args) {//test
+	public static void main(String[] args) {
 		
         DeliveryController controller = new DeliveryController();
         controller.start();
