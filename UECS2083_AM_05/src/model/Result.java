16a64572c56b047c1a0b463559fe49c24278b6fd
TEE LE XUAN <teelexuan@gmail.com>
2026-04-24 21:03:37 +0800

    fixed

--- UECS2083_AM_05/src/model/Result.java
+++ UECS2083_AM_05/src/model/Result.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package model;
 
 public class Result {
@@ -26,32 +25,3 @@ public class Result {
 
 	
 }
-=======
-package model;
-
-public class Result {
-	private String name;
-	private long time;
-	private double profit;
-	
-	public Result(String name, long time, double profit) {
-		this.name = name;
-		this.time = time;
-		this.profit = profit;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public long getTime() {
-		return time;
-	}
-
-	public double getProfit() {
-		return profit;
-	}
-
-	
-}
->>>>>>> branch 'main' of https://github.com/teehere/Job-Sequence-Problem
